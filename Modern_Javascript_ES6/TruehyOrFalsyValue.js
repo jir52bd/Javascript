b4cecfac00d1ebe70696a1ec9a3886d5ea293cb4
unknown <jirrazu123@gmail.com>
2022-09-02 11:15:10 +0600

    Truthy and Falsy Value

---
+++ Modern_Javascript_ES6/TruehyOrFalsyValue.js
@@ -0,0 +1,23 @@
+//truthy value and falsy value 
+
+//let value = true;
+
+//let value = "test";
+
+/* ther have 6 falsy valye 
+    1. ""
+    2. null
+    3. false
+    4. undefined
+    5. 0
+    6. NaN
+*/
+
+let value = 0;
+
+if(value){
+    console.log("I am truthy.");
+}else{
+    console.log("I am falsy.");
+}
+
